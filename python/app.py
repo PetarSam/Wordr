fc1326a995eae5b43f277bcb70c6547bc419e438
Petar <petarsamardzija12@gmail.com>
2020-01-25 14:43:07 +0100

    backend core

--- python/app.py
+++ python/app.py
@@ -1,28 +1,39 @@
 import bs4 as bs
 import urllib.request
 
-words = {}
-innerLinks = []
-outerLinks = []
+words = []
+freq = {}
+
+pages = 10
+link = 'https://thehub.io/jobs?positionTypes=5b8e46b3853f039706b6ea71&positionTypes=5b8e46b3853f039706b6ea72&positionTypes=5b8e46b3853f039706b6ea70&roles=backenddeveloper&roles=frontenddeveloper&roles=fullstackdeveloper&roles=androiddeveloper&roles=iosdeveloper&location=Copenhagen,%20Denmark&countryCode=DK'
+classname = 'card-job-find-list__position'
+
+def skin(elem):
+    return str(elem).split('>')[1].split('<')[0].split()
+
+def counter(words):
+    wordsSet = sorted(set(words))
+    for word in wordsSet:
+        freq[word] = words.count(word)
+    
+def souper(sauce): 
+    soup = bs.BeautifulSoup(sauce,'lxml')            
+    body = soup.find_all('span',{'class':[classname]})
+    for word in body:
+        for word in skin(word):
+            words.append(word)
 
 def main(link):
-    sauce = urllib.request.urlopen(link)
-    soup = bs.BeautifulSoup(sauce,'lxml')
-    for link in soup.find_all('a'):
-        print("links\n")
-        print(link.get('href')) 
+    if pages != 0:
+        for i in range(pages):
+            sauce = urllib.request.urlopen(link+'&page='+str(i+1))
+            souper(sauce)
+    else:
+        sauce = urllib.request.urlopen(link)
+        souper(sauce)
+    
+    counter(words)
+    new_freq = {k: v for k, v in sorted(freq.items(), key=lambda item: item[1])}
+    print(new_freq)
 
-    ### Needs to add background images
-    for source in soup.find_all('img'):
-        print("images\n")
-        print(source.get('src')) 
-    body = soup.find('body').text.strip().split()
-    for word in body:
-        if word not in words:
-            words[word] = 1
-        else:
-            words[word] += 1
-    wordsSorted = {k: v for k, v in sorted(words.items(), key=lambda item: item[1])}
-    print(wordsSorted.items())
-
-main('https://thehub.io/jobs?positionTypes=5b8e46b3853f039706b6ea71&positionTypes=5b8e46b3853f039706b6ea72&positionTypes=5b8e46b3853f039706b6ea70&roles=backenddeveloper&roles=frontenddeveloper&roles=fullstackdeveloper&roles=androiddeveloper&roles=iosdeveloper&location=Copenhagen,%20Denmark&countryCode=DK')
+main(link)
